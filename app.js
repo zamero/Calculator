6de6ae4f160fe89d43d85d71bb8e7a94cbeb61d3
unknown <samer201001@gmail.com>
2022-09-08 21:04:00 +0200

    la till lite js logik inte klar än

---
+++ app.js
@@ -0,0 +1,25 @@
+const calcOn = 0
+let add = () => x + y
+let multi = () => x * y
+let sub = () => x - y
+let hasClickedFirst = 0
+let x = 0
+let y = 0
+let one = document.querySelector(".num1")
+let two = document.querySelector(".num2")
+let three = document.querySelector(".num3")
+let four = document.querySelector(".num4")
+let five = document.querySelector(".num5")
+let six = document.querySelector(".num6")
+let seven = document.querySelector(".num7")
+let eight = document.querySelector(".num8")
+let nine = document.querySelector(".num9")
+let zero = document.querySelector(".num0")
+
+
+// if (hasClickedFirst == 0){
+//     x = 
+// }
+// while (calcOn = 0) {
+//     if 
+// }
