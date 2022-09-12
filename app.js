ffc59fd6e3376c5052951781af531ee5bd165f0b
Zamero <samer201001@gmail.com>
2022-09-12 21:10:44 +0200

    Redid the project. Added keydown och click event litseners, css etc

--- app.js
+++ app.js
@@ -1,83 +1,53 @@
-numberOnscreen = document.querySelector(".calcnum")
-let x = []
-let y = []
-let lastanswer = []
-let choice = 0
+let obj = ["Backspace", "Enter", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "*", "/", "c", ".", "(", ")", "+", "-"]
 
-let input = document.querySelectorAll(".num0, .num9, .num8, .num7, .num6, .num5, .num4, .num3, .num2, .num1")
-for (let i = 0; i < input.length; i++) {
-    input[i].onclick = function () {
-        let num = i + 1
-        if (num == 10) {
-            num = 0
+let display = document.querySelector("#display")
+let buttons = Array.from(document.querySelectorAll(".button"))
+buttons.map(button => {
+    button.addEventListener("click", (e) => {
+        switch (e.target.innerText) {
+            case "C":
+                display.innerText = ""
+                break
+            case "←":
+                display.innerText = display.innerText.slice(0, -1)
+                break
+            case "=":
+                try {
+                    display.innerText = eval(display.innerText)
+                } catch {
+                    display.innerText = "Försök igen! Error"
+                }
+
+                break
+            default:
+                display.innerText += e.target.innerText
         }
-        let sendit = num.toString()
-        if (choice == 0) {
-            x.push(sendit)
-            numberOnscreen.innerText = x.join("")
-        }
-        else {
-            y.push(sendit)
-            numberOnscreen.innerText = y.join("")
-        }
+    })
+})
 
-    }
-}
 
-let symbol = document.querySelectorAll(".add, .sub, .multi, .div")
-for (let i = 0; i < symbol.length; i++) {
-    symbol[i].onclick = function () {
-        choice = i + 1
-        console.log(choice)
-        numberOnscreen.innerText = 0
-    }
-}
+window.addEventListener("keydown", logKey)
+function logKey(e) {
+    switch (e.key) {
+        case "Enter":
+            try {
+                display.innerText = eval(display.innerText)
+            } catch {
+                display.innerText = "Försök igen! Error"
+            }
+            break
+        case "Backspace":
+            display.innerText = display.innerText.slice(0, -1)
+            break
+        case "c":
+            display.innerText = ""
+            break
+        default:
+            if (obj.includes(e.key)) {
+                display.innerText = display.innerText.concat(e.key)
+            } else {
 
-document.querySelector(".clear").onclick = function () {
-    console.log("yo")
-    x.splice(0, x.length)
-    y.splice(0, y.length)
-    lastanswer = []
-    choice = 0
-    numberOnscreen.innerText = 0
-}
-
-document.querySelector(".equals").onclick = function () {
-    if (choice == 1) {
-        let answer = parseInt(x.join("")) + parseInt(y.join(""))
-        x.splice(0, x.length)
-        y.splice(0, y.length)
-        numberOnscreen.innerText = x.join("")
-        choice = 0
-        numberOnscreen.innerText = answer
-        lastanswer.pop()
-        lastanswer.push(answer)
+            }
+            break
     }
-    else if (choice == 2) {
-        let answer = parseInt(x.join("")) - parseInt(y.join(""))
-        x.splice(0, x.length)
-        y.splice(0, y.length)
-        choice = 0
-        numberOnscreen.innerText = answer
-        lastanswer.pop()
-        lastanswer.push(answer)
-    }
-    else if (choice == 3) {
-        let answer = parseInt(x.join("")) * parseInt(y.join(""))
-        x.splice(0, x.length)
-        y.splice(0, y.length)
-        choice = 0
-        numberOnscreen.innerText = answer
-        lastanswer.pop()
-        lastanswer.push(answer)
-    }
-    else {
-        choice = 0
-        x.splice(0, x.length)
-        y.splice(0, y.length)
-        numberOnscreen.innerText = answer
-        lastanswer.pop()
-        lastanswer.push(answer)
-    }
-
 }
